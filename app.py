ca213c00b843f4523c99332b71dbec8b4024b6e3
Gary Lin <garylin@unison-ucg.com>
2023-02-15 15:49:36 -0800

    Update App and data

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from dash.exceptions import PreventUpdate
 import pandas as pd
 import pyarrow
 import fastparquet
+from tabs import tab1, tab2, tab3
 
 # App
 app = dash.Dash(
@@ -19,8 +20,8 @@ app.title="DB1B Data"
 server = app.server
 
 # Data
-path = 'https://raw.githubusercontent.com/garycl/AirportApp/master'
-df_airports = pd.read_parquet(f'{path}/data/NIPAS_Airports.parquet')
+path = 'https://raw.githubusercontent.com/garycl/AirportData/master'
+df_airports = pd.read_parquet(f'https://raw.githubusercontent.com/garycl/AirportApp/master/data/NPIAS_Airports.parquet')
 df_airports = df_airports[df_airports['SvcLvl_FY23']=='P'].sort_values(by='LocID')
 airport_options = [
     {"label": str(df_airports.loc[df_airports.LocID==airport,'LocID'].values[0]), "value": str(airport)}
@@ -35,158 +36,146 @@ variables = [
     'AvgFare'
 ]
 
-app.layout = html.Div(
-    # Layout
-    dbc.Col([
-        html.P('Select Year Range', style={'textAlign': 'center'}),
-        dcc.RangeSlider(
-            id='year_slider',
-            min=1993,
-            max=2022,
-            value=[1993, 2022],
-            marks={int(i):str(i) for i in range(1993,2022+1,1)},
-        ),
-        html.Br(),
-        html.P('Select Airports', style={'textAlign': 'center'}),
-        dcc.Dropdown(
-            id='origin_dropdown',
-            value=[],
-            multi=True,
-            options=airport_options
+# App set-up
+app=dash.Dash(
+    __name__, 
+    external_stylesheets=[dbc.themes.FLATLY],
+    meta_tags=[{"name": "viewport", "content": "width=device-width"}],
+    suppress_callback_exceptions=True
+)
+app.title="Airpprt Data"
+server=app.server
 
-        ),
-        html.Br(),
-        html.P('Frequency', style={'textAlign': 'center'}),
-        dcc.RadioItems(
-            id='time_radio',
-            options = [dict(label = 'Quarter', value = 'Quarter'),
-                      dict(label = 'Annual', value = 'Annual')],
-            value='Quarter',
-            style={'textAlign': 'center'},
-            labelStyle={'display': 'inline-block'}
-        ),
-        html.Button('Submit', id='button'),
-        html.Div([
-            html.Br(),
-            html.Div(id="table-1")
-        ]),
-    ]),
-    id="mainContainer",
-    style={"display": "flex", "flex-direction": "column"},
+# Sidebar Style
+SIDEBAR_STYLE={
+    'position': 'fixed',
+    'top': 0,
+    'left': 0,
+    'bottom': 0,
+    'width': '20%',
+    'padding': '20px 10px',
+    'background-color': '#f8f9fa'
+}
 
+# Content Style
+CONTENT_STYLE={
+    'float':'left',
+    'margin-left':'5%',
+    'margin-right':'5%',
+    'top': 0,
+    'padding': '20px 10px'
+}
+
+tab_style = {
+    'background-color': '#f8f9fa',
+    'border-color':'#1b9e77',
+    #'text-transform': 'uppercase',
+    'font-size': '24px',
+    'font-weight': 600,
+    'align-items': 'center',
+    'padding':'40px'
+}
+
+tab_selected_style = {
+    'border-color':'#1b9e77',
+    'color':'#1b9e77',
+    #'text-transform': 'uppercase',
+    'font-size': '24px',
+    'font-weight': 2000,
+    'align-items': 'center',
+    'padding':'40px'
+}
+
+controls=dbc.Col([
+    html.Br(),
+    html.Br(),
+    html.P('Select Start Year', style={'textAlign': 'center'}),
+    dcc.Dropdown(
+        id='start_year',
+        value=None,
+        options=[{"label":str(i),"value":str(i)} for i in range(1993,2023,1)]
+    ),
+    html.Br(),
+    html.P('Select End Year', style={'textAlign': 'center'}),
+    dcc.Dropdown(
+        id='end_year',
+        value=None,
+        options=[{"label":str(i),"value":str(i)} for i in range(1993,2023,1)]
+    ),
+    html.Br(),
+    html.P('Select Airports', style={'textAlign': 'center'}),
+    dcc.Dropdown(
+        id='origin_dropdown',
+        value=None,
+        multi=True,
+        options=airport_options
+    ),
+    html.Br(),
+    html.P('Frequency', style={'textAlign': 'center'}),
+    dcc.RadioItems(
+        id='time_radio',
+        options = [dict(label = 'Quarter', value = 'Quarter'),
+                 dict(label = 'Annual', value = 'Annual')],
+        value='Quarter',
+        style={'textAlign': 'center'},
+    ),
+    html.Button('Submit', id='button'),
+]),
+
+sidebar=html.Div(
+    controls,
+    style=SIDEBAR_STYLE
+)
+content=html.Div(
+    [
+        html.Br(),
+        html.Div(id="table-1")
+    ],
+    style=CONTENT_STYLE
 )
 
-variables = [
-    'PAX','FaredPAX',
-    'MktFare', 'MktMilesFlown', 
-    'PWMktFare', 'PWMktMilesFlown', 
-    'FareYield', 'PWFareYield',
-    'AvgFare'
-]
-
-def get_yields(data):
-    df = data.copy()
-    df['FareYield'] = df['MktFare'].values / df['MktMilesFlown'].values
-    df['PWFareYield']= df['PWMktFare'].values / df['PWMktMilesFlown'].values
-    df['AvgFare'] = df['MktFare'].values / df['FaredPAX'].values
-    return df
-
-def get_data(df, time, airport):
-    if time=='Quarter':
-        group_var = ['Origin','Period']
-    else:
-        group_var = ['Origin','Year']
-    tdf = df[(df.Origin==airport) | (df.Dest==airport)].copy()
-    tdf['Origin']=airport
-    tdf = tdf[group_var + variables]
-    tdf = tdf.groupby(group_var).sum().reset_index()
-    tdf = get_yields(tdf)
-    tdf = tdf[group_var + variables]
-    return tdf
-
-def create_data(year_range, origin, time):
-    min = year_range[0]
-    max = year_range[1]
-    data  = pd.DataFrame()
-    for year in range(min, max+1):
-        df = pd.read_parquet(f'{path}/data/dbmkt_{year}.parquet')
-        df = df.rename(columns={'Passengers':'PAX'})
-        df['Period'] = df['Year'].astype(str) + 'Q' + df['Quarter'].astype(str)
-        df.loc[df.Origin=='HHH', 'Origin']='HXD'
-        df.loc[df.Dest=='HHH', 'Dest']='HXD'
-        df = df[
-            (df.Origin.isin(origin)) | 
-            (df.Dest.isin(origin))
-        ]
-        for i in origin:
-            tdf = get_data(df, time, i)
-            data = pd.concat([data, tdf], ignore_index=True)
-    return data
-
-
-# Table 1 callback
-@app.callback(
-    #Output('graph-1', 'figure'),
-    Output('table-1', 'children'),
+# Describe the layout/ UI of the app
+app.layout=html.Div(
     [
+        dcc.Tabs(
+            id="tabs", 
+            value='tab-1', 
+            children=[
+                dcc.Tab(label='Fare & Yields', value='tab-1', style=tab_style, selected_style=tab_selected_style),
+                dcc.Tab(label='Origin & Destination', value='tab-2', style=tab_style, selected_style=tab_selected_style),
+                dcc.Tab(label='Top O&D', value='tab-3', style=tab_style, selected_style=tab_selected_style),
+            ]
+        ),
+        html.Div(id='tabs-content'),
+        html.Div(
+            dbc.Row([
+                dbc.Col(sidebar, md=1), 
+                dbc.Col(content, md=3)
+            ])
+        )
+
+    ],
+    style={'margin-left': '20%'}
+)
+
+@app.callback(
+    Output('tabs-content', 'children'),
+    [
+        Input('tabs', 'value'),
         Input('button', 'n_clicks'),
-        State('year_slider', 'value'),
+        State('start_year', 'value'),
+        State('end_year', 'value'),
         State('origin_dropdown', 'value'),
         State('time_radio', 'value'),
     ]
 )
-
-# ------------------------------------------------------------------------------
-# Define table
-# ------------------------------------------------------------------------------
-def update_table(n_clicks, year_slider, origin, time):
-    if n_clicks is None:
-        return dash.no_update
-    else:
-        table=create_data(
-            year_slider, 
-            origin,
-            time
-        )
-        if time=='Quarter':
-            group_var = [
-                {"name": "Airport", "id": "Origin"},
-                {"name": "Period", "id": "Period"},
-            ]
-            table = table.sort_values(by=['Origin','Period'])
-        else:
-            group_var = [
-                {"name": "Airport", "id": "Origin"},
-                {"name": "Year", "id": "Year"},
-            ]
-            table = table.sort_values(by=['Origin','Year'])
-
-        table=dash_table.DataTable(
-                columns= group_var +[
-                    {"name": "PAX", "id": "PAX", "type":'numeric', "format":Format().group(True)},
-                    {"name": "FaredPAX", "id": "FaredPAX", "type":'numeric', "format":Format().group(True)},
-                    {"name": "MktFare", "id": "MktFare", "type":'numeric', "format":FormatTemplate.money(2)},
-                    {"name": "MktMilesFlown", "id": "MktMilesFlown", "type":'numeric', "format":Format().group(True)},
-                    {"name": "PWMktFare", "id": "PWMktFare", "type":'numeric', "format":FormatTemplate.money(2)},
-                    {"name": "PWMktMilesFlown", "id": "PWMktMilesFlown", "type":'numeric', "format":Format().group(True)},
-                    {"name": "FareYield", "id": "FareYield", "type":'numeric', "format":FormatTemplate.money(4)},
-                    {"name": "PWFareYield", "id": "PWFareYield", "type":'numeric', "format":FormatTemplate.money(4)},
-                    {"name": "AvgFare", "id": "AvgFare", "type":'numeric', "format":FormatTemplate.money(2)},
-                ],
-                data=table.to_dict('records'),
-                fixed_rows={'headers': True},
-                style_table={'height': 400},  # defaults to 500
-                style_cell={
-                    'fontSize':16, 
-                    'font-family':'Calibri', 
-                    'textAlign':'right',
-                },
-                style_header={
-                    'fontWeight': 'bold', 
-                },
-                export_format="csv"
-            )
-        return  table
+def render_content(tab, button, start_year, end_year, origin, time):
+    if tab == 'tab-1':
+        return tab1.create_layout(button, start_year, end_year, origin, time)
+    elif tab=='tab-2':
+        return tab2.create_layout(button, start_year, end_year, origin, time)
+    elif tab=='tab-3':
+        return tab3.create_layout(button, start_year, end_year, origin, time)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
